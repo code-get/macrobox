a83014a7d42dd5b30f591920aee4a28978e5bfc9
C.Landry <macromantic@outlook.com>
2018-09-21 16:32:56 -0400

    updates

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,40 +6,26 @@ function installVIM() {
    echo 'syntax on' >> '/home/vagrant/.vimrc'
 }
 
-function installGit() {
-   gitver=$1
-
+function installPerl() {
+   yum install -y epel-release;
+   yum install -y git;
    yum install -y wget
    yum install -y gcc autoconf libcurl-devel expat-devel
    yum install -y gettext-devel openssl-devel perl-devel 
    yum install -y zlib-devel xmlto util-linux docbook-utils
-   yum install -y asciidoc cpan perl-App-cpanminus
-
-   cpanm Test::Simple
-   cpanm Fatal
-   cpanm XML::SAX
-
-   olddir=`pwd`
-   mkdir -p /usr/local/downloads
-   cd /usr/local/downloads
-   wget "https://www.kernel.org/pub/software/scm/git/git-$gitver.tar.gz"
-   tar -zxf "git-$gitver.tar.gz"
-   cd "git-$gitver"
-   make configure
-   ./configure --prefix=/usr
-   make all doc
-   make install install-doc install-html
+   yum install -y perl cpan perl-App-cpanminus
 }
 
 function installNGINX() {
 	yum install -y wget
+	useradd 'www-data'
 
 	sh -c 'echo -e "[nginx]\nname=nginix repo\nbaseurl=http://nginx.org/packages/rhel/7/x86_64/\ngpgcheck=1\nenabled=1" > /etc/yum.repos.d/nginx.repo'
-
 
 	wget https://nginx.org/keys/nginx_signing.key
 	rpm --import nginx_signing.key
 
+	yum clean all
 	yum check-update -y
 	yum install nginx -y
 }
@@ -53,27 +39,61 @@ function configNGINX() {
 	webroot=/vagrant/www
 	echo "Creating Web Root $webroot"
 	mkdir -p $webroot
-	chown -R root:root $webroot
+	chown -R www-data:www-data $webroot
 	chmod -R 755 $webroot
 	chcon -R -h system_u:object_r:httpd_sys_content_t:s0 $webroot
 
 	nginxconf=/etc/nginx/nginx.conf
-	nginxconfsettings="http {\n    server {\n        server_name $hostname;\n        root $webroot/;\n        location /{\n            #\n        }\n    }\n"
-	sed -i -e "s@http {@$nginxconfsettings@g" $nginxconf
+	cat /vagrant/resources/nginx-perl.conf >$nginxconf
+
+	#nginxsvc=/usr/lib/systemd/system/nginx.service
+	#cat /vagrant/resources/nginx.service >$nginxsvc
 
 	echo "Reloading Nginx"
 	nginx -s reload
 	chkconfig nginx on
 	systemctl restart nginx
 }
+function installFastCGIWrap() {
+    yum install -y epel-release
+	yum install -y fcgi-devel spawn-fcgi
+	yum install -y automake
+	olddir=`pwd`
+    mkdir -p /usr/local/downloads
+    cd /usr/local/downloads
+	git clone git://github.com/gnosek/fcgiwrap.git
+	cd fcgiwrap
+	autoreconf -i
+	./configure
+	make
+	make install
+	cd $olddir
 
+	fcgiconf=/etc/sysconfig/spawn-fcgi
+	cat /vagrant/resources/spawn-fcgi >$fcgiconf
+
+	chkconfig --levels 235 spawn-fcgi on
+	/etc/init.d/spawn-fcgi start
+	
+	cp /usr/local/downloads/fcgiwrap/systemd/fcgiwrap.socket /var/run/.
+	chown www-data:www-data /var/run/fcgiwrap.socket
+	chcon -h system_u:object_r:httpd_var_run_t:s0 /var/run/fcgiwrap.socket
+	
+	#fcgisvc=/usr/lib/systemd/system/fcgiwrap.service
+	#cat /vagrant/resources/fcgiwrap.service >$fcgisvc
+
+	#systemctl enable fcgiwrap
+	#systemctl start fcgiwrap
+}
 function allowPasswordAuth() {
    # Configure Vagrant Security
    sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
    systemctl start sshd
 }
 
-allowPasswordAuth
-installNGINX
+#allowPasswordAuth
+#installPerl
+#installNGINX
+installFastCGIWrap
 configNGINX 'macrobox' '/etc/hosts'
-installVIM
+#installVIM
--- www/cgi-bin/index.cgi
+++ www/cgi-bin/index.cgi
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -wT
-
-my $time	    = localtime;
+ 
+my $time        = localtime;
 my $remote_id   = $ENV{REMOTE_HOST} || $ENV{REMOTE_ADDR};
 my $admin_email = $ENV{SERVER_ADMIN};
-
+ 
 print "Content-type: text/html\n\n";
 print <<END_OF_PAGE;
 <!DOCTYPE html>
@@ -12,7 +12,8 @@ print <<END_OF_PAGE;
    <title>Welcome</title>
 </head>
 <body>
-	<h1>Welcome</h1>
+   <h1>Welcome</h1>
 </body>
 </html>
 END_OF_PAGE
+
