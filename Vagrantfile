073d0c8f5a6a9ec4b08bd01cda5f0523bfb8a9ee
c.landry <macromantic@outlook.com>
2018-09-22 11:46:01 -0400

    fixed

--- Vagrantfile
+++ Vagrantfile
@@ -2,8 +2,8 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "centos/7"
+  config.vm.box = "centos/6"
   config.vm.hostname = "macrobox"
-  config.vm.network "forwarded_port", guest: 8080, host: 8888
+  config.vm.network "forwarded_port", guest: 8080, host: 8899
   config.vm.provision "shell", path: "scripts/init.sh"
 end
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,80 +1,38 @@
 #!/bin/bash
 
-function installVIM() {
-   yum install -y vim 
-   echo 'set number' > '/home/vagrant/.vimrc'
-   echo 'syntax on' >> '/home/vagrant/.vimrc'
-}
+setenforce 0
+sed -i -e 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 
-function installDevLibs() {
-   yum install -y epel-release;
-   yum install -y git;
-   yum install -y wget
-   yum install -y gcc autoconf libcurl-devel expat-devel
-   yum install -y gettext-devel openssl-devel perl-devel 
-   yum install -y zlib-devel xmlto util-linux docbook-utils
-   yum install -y perl cpan perl-App-cpanminus
-}
+yum update -y
+yum groupinstall -y 'Development Tools'
+yum install -y epel-release
+yum install -y nginx
+yum install -y fcgi-devel
 
-function installNGINX() {
-	yum install -y wget
-	sh -c 'echo -e "[nginx]\nname=nginix repo\nbaseurl=http://nginx.org/packages/rhel/7/x86_64/\ngpgcheck=1\nenabled=1" > /etc/yum.repos.d/nginx.repo'
+# fcgiwrap
+oldpwd=`pwd`
+cd /usr/local/src
+git clone git://github.com/gnosek/fcgiwrap.git
+cd fcgiwrap
+autoreconf -i
+./configure
+make
+make install
+cd $oldpwd
 
-	wget https://nginx.org/keys/nginx_signing.key
-	rpm --import nginx_signing.key
+# spawn-fcgi
+yum install -y spawn-fcgi
+spawnfcgiconf=/etc/sysconfig/spawn-fcgi
+cat /vagrant/resources/spawn-fcgi >$spawnfcgiconf
+chkconfig --levels 235 spawn-fcgi on
 
-	yum clean all
-	yum check-update -y
-	yum install nginx -y
-}
-function configNGINX() {
-	hostname=$1
-	hostsfile=$2
+# nginx config
+nginxconf=/etc/nginx/nginx.conf
+cat /vagrant/resources/nginx-perl.conf >$nginxconf
 
-	echo "Creating Host $hostname"
-	webroot=/vagrant/www
-	echo "Creating Web Root $webroot"
-	mkdir -p $webroot
-	chown -R nginx:nginx $webroot
-	chmod -R 755 $webroot
-	chcon -R -h system_u:object_r:httpd_sys_content_t:s0 $webroot
+# www config
+chmod -R 755 /vagrant/www
+chown -R nginx:nginx /vagrant/www
 
-	nginxconf=/etc/nginx/nginx.conf
-	cat /vagrant/resources/nginx-perl.conf >$nginxconf
-
-	echo "Reloading Nginx"
-	nginx -s reload
-	chkconfig nginx on
-	systemctl restart nginx
-}
-function installFastCGIWrap() {
-    yum install -y epel-release
-	yum install -y fcgi-devel spawn-fcgi
-	yum install -y automake
-	olddir=`pwd`
-    mkdir -p /usr/local/downloads
-    cd /usr/local/downloads
-	git clone git://github.com/gnosek/fcgiwrap.git
-	cd fcgiwrap
-	autoreconf -i
-	./configure
-	make
-	make install
-	cd $olddir
-
-	fcgiconf=/etc/sysconfig/spawn-fcgi
-	cat /vagrant/resources/spawn-fcgi >$fcgiconf
-
-	chkconfig --levels 235 spawn-fcgi on
-	/etc/init.d/spawn-fcgi start
-	
-	cp /usr/local/downloads/fcgiwrap/systemd/fcgiwrap.socket /var/run/.
-	chown nginx:nginx /var/run/fcgiwrap.socket
-	chcon -h system_u:object_r:httpd_var_run_t:s0 /var/run/fcgiwrap.socket
-}
-
-installDevLibs
-installNGINX
-configNGINX
-installVIM
-installFastCGIWrap
+/etc/init.d/nginx start
+/etc/init.d/spawn-fcgi start
